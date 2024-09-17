788935ec076ebeb0320f7f6ac832b278c8d12d1f
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-09-17 16:48:51 -0300

    upload inicial do projeto

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Compras_Proauto
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
